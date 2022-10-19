f75d6e24c25008316fb66797578643b0fc567c4a
David <4292@holbertonschool.com>
2022-10-19 17:05:13 -0400

    adding semicolongs

--- alphabet.js
+++ alphabet.js
@@ -1,5 +1,5 @@
-let alphabet = []
+let alphabet = [];
 for (let i = 97; i < 123; i++) {
     alphabet.push(String.fromCharCode(i))
-}
-console.log(alphabet.join().replace(/,/g, ''))
+};
+console.log(alphabet.join().replace(/,/g, ''));
