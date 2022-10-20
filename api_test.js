afc20367b0dcb6761eb612e7e3ff93d1969673ff
David <4292@holbertonschool.com>
2022-10-20 12:20:15 -0400

    api test

--- api_test.js
+++ api_test.js
@@ -1,15 +1,9 @@
-var axios = require('axios');
-
-var config = {
-  method: 'get',
-  url: 'https://jsonplaceholder.typicode.com/users',
-  headers: { }
-};
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://jsonplaceholder.typicode.com/users", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
