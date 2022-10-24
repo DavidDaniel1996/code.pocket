e3b29e0830c00bff9cd37a023e5fdebd6efa79f7
David <4292@holbertonschool.com>
2022-10-24 12:03:55 -0400

    fixed rounding algorithm

--- tipping_roundUp.js
+++ tipping_roundUp.js
@@ -1,15 +1,19 @@
 let total = 25.00;
 let percent = .17;
-let roundTarget = .77;
+let roundTarget = .02;
 
 let baseTip = total * percent
 
-let trueTip;
+let roundedTip;
 if (baseTip % 1 === 0) {
-    trueTip = baseTip + roundTarget
+    roundedTip = baseTip + roundTarget
 } else {
-    trueTip = Math.ceil(baseTip) - (1.00 - roundTarget)
+    roundedTip = Math.ceil(baseTip) - (1.00 - roundTarget)
+}
+
+if (roundedTip < baseTip) {
+    roundedTip += 1
 }
 console.log(`Base Tip: ${baseTip.toFixed(2)}`)
-console.log(`Rounded Tip: ${trueTip.toFixed(2)}`)
-console.log(`Total to pay: ${(total + trueTip).toFixed(2)}`)
+console.log(`Rounded Tip: ${roundedTip.toFixed(2)}`)
+console.log(`Total to pay: ${(total + roundedTip).toFixed(2)}`)
