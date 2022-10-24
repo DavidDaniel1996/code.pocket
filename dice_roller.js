06faff077b611dba79b2a0f9a66800f7adfaa4d8
David <4292@holbertonschool.com>
2022-10-24 13:43:56 -0400

    Added formatting

--- dice_roller.js
+++ dice_roller.js
@@ -2,4 +2,4 @@ let dice = 20;
 
 let min = Math.ceil(dice - (dice - 1));
 let max = Math.floor(dice);
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(`Result: ${Math.floor(Math.random() * (max - min + 1)) + min}`);
